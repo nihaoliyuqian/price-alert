f9d828b5943a6641d5e664faa55856b7b1485d5f
nihaoliyuqian <79790928+nihaoliyuqian@users.noreply.github.com>
2021-03-23 23:23:03 +0800

    Update database.py

--- src/common/database.py
+++ src/common/database.py
@@ -11,7 +11,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client["microblog"]
+        Database.DATABASE = client["price_alert"]
 
     @staticmethod
     def insert(collection, data):
